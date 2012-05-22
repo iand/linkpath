ceeddb7b5e96fbefae9e904fc3ad7896475ee815
Ian Davis <me@iandavis.com>
2012-05-22 16:16:29 +0100

    Cleanup of disk, this commit just preserves latest changes without verification

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 setup(
     name = 'linkpath',
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'A Python library for querying the linked data web',
     author='Ian Davis',
     author_email='nospam@iandavis.com',
     url='https://github.com/iand/linkpath',
     classifiers=['Programming Language :: Python','License :: Public Domain', 'Operating System :: OS Independent', 'Development Status :: 4 - Beta', 'Intended Audience :: Developers', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Database'],
     packages =['linkpath'],
-    install_requires=['httplib2', 'rdflib'],
+    install_requires=['httplib2', 'rdflib>=3.1.0'],
     scripts = ["scripts/linkpath"],
     
 )
